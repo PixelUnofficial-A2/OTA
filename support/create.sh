09378192985961b7bc5b5a534b7c2c3d704f60f5
Immanuel Raj <immanuelr44@gmail.com>
2020-10-16 16:51:24 +0530

    update for server

--- support/create.sh
+++ support/create.sh
@@ -1,11 +1,11 @@
-ROMDIR=~/crom/pe10
+ROMDIR=~/rom/pe
 cd $ROMDIR
 DATETIME=$(grep "org.pixelexperience.build_date_utc=" out/target/product/jasmine_sprout/system/build.prop | cut -d "=" -f 2)
 FILENAME=$(find out/target/product/jasmine_sprout/PixelExperience*.zip | cut -d "/" -f 5)
 ID=$(md5sum out/target/product/jasmine_sprout/PixelExperience*.zip | cut -d " " -f 1)
 FILEHASH=$ID
 SIZE=$(wc -c out/target/product/jasmine_sprout/PixelExperience*.zip | awk '{print $1}')
-URL1="https://sourceforge.net/projects/pixelexperienceunofficial-mia2/files/builds/$FILENAME/download"
+URL1="https://sourceforge.net/projects/teamneon-ports/files/Pixel-Experience/$FILENAME/download"
 URL=$URL1
 VERSION="ten"
 DONATE_URL="https://www.paypal.me/immanuelr44"
@@ -15,4 +15,4 @@ MAINTAINER="Immanuel Raj"
 MAINTAINER_URL="https:\/\/t.me/Immanuel_Raj"
 FORUM_URL="https://forum.xda-developers.com/mi-a2/development/rom-pixel-experience-t4138459"
 JSON_FMT='{\n"error":false,\n"filename": %s,\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s",\n"donate_url": "%s",\n"website_url":"%s",\n"news_url":"%s",\n"maintainer":"%s",\n"maintainer_url":"%s",\n"forum_url":"%s"\n}'
-printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > /home/raj/Desktop/OTA/builds/jasmine_sprout.json
+printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > 
